03db875aac09dd4da70e12c1705dbafe7551f6c0
Miguel231 <miguelpitufet@gmail.com>
2024-10-10 21:23:41 +0200

    uu

--- licenseplate.py
+++
@@ -1,8 +0,0 @@
-import functions_licenseplate as fl
-import functions_yolo_licenseplate as yolo
-
-
-image_folder = 'G:/Mi unidad/LICENSE_PLATES_RECOGITION_L&V/combined_folder_last(onlyspain)'
-
-#fl.plate_recognition(image_folder= image_folder)
-yolo.yolo_plate_recognition(image_folder=image_folder)
